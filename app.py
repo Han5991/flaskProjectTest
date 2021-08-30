01e722bfdf9fd69041b1480759edc817727fb64f
rewq985 <bnbt3@naver.com>
2021-08-30 21:16:13 +0900

    모델을 다루는 sqlalchemy 추가함

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask , render_template
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Test(db.Model):
+    __tableName__ = 'test_table'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+
+
+db.create_all()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('index.html')
 
 
