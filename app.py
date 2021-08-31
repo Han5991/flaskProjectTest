06f94076438e01a84c217bd85e2a4a189e713b6c
rewq985 <bnbt3@naver.com>
2021-08-31 19:54:33 +0900

    진자2를 통한 템플릿 활용 방법

--- app.py
+++ app.py
@@ -3,22 +3,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-dbfile = os.path.join(basedir, 'db.sqlite')
+dbFile = os.path.join(basedir, 'db.sqlite')
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbFile
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-
-class Test(db.Model):
-    __tableName__ = 'test_table'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
-
-
-db.create_all()
 
 
 @app.route('/')
